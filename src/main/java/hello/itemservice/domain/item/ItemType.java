97f06d5fceb033e8fb42b9c5e2cb58f50795a210
bookck13 <bookck13@gmail.com>
2023-06-24 15:52:48 -0400

    Initial commit

---
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,12 @@
+package hello.itemservice.domain.item;
+
+public enum ItemType {
+
+    BOOK("book"), FOOD("food"), ETC("etc");
+
+    private final String description;
+
+    ItemType(String description) {
+        this.description = description;
+    }
+}
